40ff3e66892cb6110b8469de0e707acc827fdbd5
ubai-d <ubaidurrehman14827@gmail.com>
2024-03-20 20:13:14 +0500

    initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// function halfFryEgg():number{
+//     let cooked:number = 1 + 1.5 +2;
+//     //                 salt + butter + egg
+//     // console.log(cooked);
+//     return cooked
+// }
+// const respnse = halfFryEgg(2,4 ,10)
+// console.log(respnse);
+// // invoking function
+function myTeachers(teacher1, teacher2, teacher3, teacher4) {
+    return teacher1 + teacher2 + teacher3 + teacher4;
+}
+var response = myTeachers("Asharib", "Ubaid", "Hafsa", "Naeem");
+console.log(response);
---
+++ main.ts
@@ -0,0 +1,23 @@
+// function halfFryEgg():number{
+//     let cooked:number = 1 + 1.5 +2;
+//     //                 salt + butter + egg
+//     // console.log(cooked);
+//     return cooked
+// }
+
+// const respnse = halfFryEgg(2,4 ,10)
+// console.log(respnse);
+
+// // invoking function
+
+
+
+
+
+
+function myTeachers(teacher1:string,teacher2:string,teacher3:string,teacher4:string){
+    return teacher1 + teacher2 + teacher3 + teacher4
+}
+
+const response = myTeachers("Asharib","Ubaid","Hafsa","Naeem")
+console.log(response);
